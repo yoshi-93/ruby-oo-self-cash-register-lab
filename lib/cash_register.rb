0cc8ee1c390abe68e52b604972db59a410e5b9c8
Mario Lopez <ez0212sp@icloud.com>
2021-01-18 19:47:59 -0500

    commit tree 37e408e25cd42fedf2c6de0de80e955fd97d420a

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
-
+class CashRegister
+    attr_accessor :total, :discount, :quantity, :price
+  
+    def initialize(discount = 0)
+      @total = 0
+      @discount = discount
+      @price = price
+      @quantity = quantity
+      @items = []
+      @transactions = []
+    end
+  
+    def add_item(title, price, quantity = 1)
+      @total += price * quantity
+      @transactions << price
+      i = quantity
+      until i == 0 do
+        @items << title
+        i -= 1
+      end
+    end
+  
+    def apply_discount
+      if @discount == 0
+        "There is no discount to apply."
+      else
+        self.total -= (0.01 * @discount * @total).to_i
+        "After the discount, the total comes to $#{self.total}."
+      end
+    end
+  
+    def items
+      @items
+    end
+  
+    def void_last_transaction
+      self.total = @total - @transactions.pop
+    end
+  end
